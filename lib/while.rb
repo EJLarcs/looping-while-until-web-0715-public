a08e9d74c6a5d54ad587959316374dcd56aaf40d
EJLarcs <ejlarcombe@gmail.com>
2015-07-30 12:46:07 -0400

    While and Until Adds

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,13 @@
 def using_while
 	levitation_force = 6
-	
+	while levitation_force < 10 
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end
 	#your code here
 end
 
-
+# * Fill out the content of the `using_while` 
+#method so that calling it will `puts` the desired phrase while your 
+#levitation force is less than `10`. Remember, every time you
+#{}`puts` the phrase, you should increment your levitation force by `1`. 
